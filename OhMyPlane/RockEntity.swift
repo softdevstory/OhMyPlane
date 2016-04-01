0b50ef278ba45202d2b3253b657058a959aaf876
softdevstory <developer@softdevstory.net>
2016-04-01 16:53:41 +0900

    RockEntity's texture is shared.

--- OhMyPlane/RockEntity.swift
+++ OhMyPlane/RockEntity.swift
@@ -14,15 +14,63 @@ enum RockType: Int {
     case Bottom
 }
 
+class RockEntityTexture {
+    static var topTextures: [String: SKTexture] = [:]
+    static var bottomTextures: [String: SKTexture] = [:]
+    
+    class func getTexture(rockType: RockType, backgroundType: BackgroundType) -> SKTexture {
+        switch rockType {
+        case .Top:
+            return getTopTexture(backgroundType)
+            
+        case .Bottom:
+            return getBottomTexture(backgroundType)
+        }
+    }
+    
+    class func getPhysicsTexture(rockType: RockType) -> SKTexture {
+        switch rockType {
+        case .Top:
+            return SKTexture(imageNamed: "obstacle_top_physics")
+            
+        case .Bottom:
+            return SKTexture(imageNamed: "obstacle_bottom_physics")
+        }
+    }
+
+    private class func getTopTexture(backgroundType: BackgroundType) -> SKTexture {
+        if let texture = topTextures[backgroundType.rawValue] {
+            return texture
+        }
+        
+        let spriteName = "obstacle_\(backgroundType.rawValue)_top"
+        let texture = SKTexture(imageNamed: spriteName)
+        
+        topTextures[backgroundType.rawValue] = texture
+        
+        return texture
+    }
+    
+    private class func getBottomTexture(backgroundType: BackgroundType) -> SKTexture {
+        if let texture = bottomTextures[backgroundType.rawValue] {
+            return texture
+        }
+        
+        let spriteName = "obstacle_\(backgroundType.rawValue)_bottom"
+        let texture = SKTexture(imageNamed: spriteName)
+        
+        bottomTextures[backgroundType.rawValue] = texture
+        
+        return texture
+    }
+}
+
 class RockEntity: GKEntity {
     var rockType: RockType
     
     var spriteComponent: SpriteComponent!
     let backgroundType: BackgroundType
 
-    var spriteName: [String]!
-    var physicsBodyFileName = ["obstacle_top_physics", "obstacle_bottom_physics"]
-    
     /*
      * position is left, bottom point.
      */
@@ -32,8 +80,7 @@ class RockEntity: GKEntity {
         
         super.init()
 
-        spriteName = ["obstacle_\(backgroundType.rawValue)_top", "obstacle_\(backgroundType.rawValue)_bottom"]
-        let texture = SKTexture(imageNamed: spriteName![rockType.rawValue])
+        let texture = RockEntityTexture.getTexture(rockType, backgroundType: backgroundType)
         
         spriteComponent = SpriteComponent(entity: self, texture: texture, size: texture.size())
         addComponent(spriteComponent)
@@ -47,7 +94,7 @@ class RockEntity: GKEntity {
         sprite.name = SpriteName.rockObstacle
         sprite.position = CGPoint(x: position.x + sprite.size.width / 2, y: position.y + sprite.size.height / 2)
         
-        let physicsBodyTexture = SKTexture(imageNamed: physicsBodyFileName[rockType.rawValue])
+        let physicsBodyTexture = RockEntityTexture.getPhysicsTexture(rockType)
         sprite.physicsBody = SKPhysicsBody(texture: physicsBodyTexture, size: physicsBodyTexture.size())
         sprite.physicsBody?.dynamic = false
         sprite.physicsBody?.categoryBitMask = PhysicsCategory.Obstacle
